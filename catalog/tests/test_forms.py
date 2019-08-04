d35bddfdfd6abff40e052f15796d3f096a109351
Huu Nhon(David) Tran <49850772+davidtranit80@users.noreply.github.com>
2019-08-04 21:59:42 +0930

    Reverse original code

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -15,7 +15,7 @@ class RenewBookFormTest(TestCase):
         date = datetime.date.today() - datetime.timedelta(days=1)
         form_data = {'renewal_date': date}
         form = RenewBookForm(data=form_data)
-        self.assertTrue(form.is_valid())
+        self.assertFalse(form.is_valid())
 
     def test_renew_form_date_too_far_in_future(self):
         """
